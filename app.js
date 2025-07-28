39569cf9ec1e127133f06fa46bc3f0dea2606d95
athen <ayoslinthen@gmail.com>
2025-07-28 16:32:50 -0400

    Closes #2 fetch books from api and render to DOM

--- app.js
+++ app.js
@@ -5,34 +5,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Select DOM elements
 const booksList = document.querySelector('#books');
-const bookTitle = document.querySelector('#bookTitle').value;
-const bookAuthor = document.querySelector('#bookAuthor').value;
-const bookPublisher = document.querySelector('#bookPublisher').value;
+const bookTitle = document.querySelector('#bookTitle');
+const bookAuthor = document.querySelector('#bookAuthor');
+const bookPublisher = document.querySelector('#bookPublisher');
 const addBookButton = document.querySelector('#addBookButton');
 
 
-const booksUrl = 'https://bookstore-api-six.vercel.app/api/books'
+const booksApi = 'https://bookstore-api-six.vercel.app/api/books/'
 
 // Add event listener for the button
 addBookButton.addEventListener('click', addBook);
 
-// Wait for the DOM to be fully loaded before running the script
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Page loaded!');
-});
 // Fetch books from the API
-function getBooks() {
-    fetch(booksUrl)
-    .then(response => response.json())
-    .then(data => {
-        // TODO: 
-        displayBooks(data);
-        console.log('Books fetched:', data);
-    })
-    .catch(error => console.error('Error fetching books:', error));
-        
+const requestOptions = {
+  method: "GET",
+  redirect: "follow"
+};
+async function fetchBooks() {
+try {
+  const response = await fetch(booksApi, requestOptions);
+  const result = await response.json();
+  console.log(result)
+  for (const book of result) {
+    const bookItem = document.createElement('li');
+    bookItem.innerHTML = `<h3>${book.title}</h3><p>Author: ${book.author}</p><p>Publisher: ${book.publisher}</p>`;
+    booksList.appendChild(bookItem);
+  }
+  console.log('Books fetched successfully');
+} catch (error) {
+  console.error(error);
+};
 }
-
+fetchBooks()
 // Add a book to the bookstore
 function addBook(book) {
     fetch(booksUrl, {
@@ -54,7 +58,4 @@ function addBook(book) {
 // TODO: fave a book
 // TODO: 
 
-// TODO: Funtionc to update the UI
-function displayBooks(data) {
-    book
-}
+
